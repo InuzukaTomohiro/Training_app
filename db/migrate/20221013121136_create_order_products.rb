cf8535972f702799490ae0d123da326978cd32c2
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-10-13 12:31:32 +0000

    [start]Traininng

---
+++ db/migrate/20221013121136_create_order_products.rb
@@ -0,0 +1,11 @@
+class CreateOrderProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_products do |t|
+      t.integer :order_id, null: false
+      t.integer :product_id, null: false
+      t.integer :amount, null: false
+
+      t.timestamps
+    end
+  end
+end
