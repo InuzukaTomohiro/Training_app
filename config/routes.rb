e999089894008be99819f8b9aabdd73b1bcecb6d
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-10-15 13:15:02 +0000

    [add]training

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :custmers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- 
- 
+
+  devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+
 
 end
