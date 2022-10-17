b5dae8992012de054bdab9511f5383c79752cfb6
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-10-17 12:34:25 +0000

    [add]training

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ApplicationRecord
   enum payment: { credit_card: 0, transfer: 1 }
   enum order_status: { wait_pay: 0, look_pay: 1, making: 2, send_ready: 3, sent: 4 }
 
+  belongs_to :customer
+  has_many :order_items
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,7 @@ class OrderItem < ApplicationRecord
 
   enum manufacture_status: { no_tach: 0, make_wating: 1, maiking: 2, complete: 3 }
 
+  belongs_to :order
+  belongs_to :item
+
 end
